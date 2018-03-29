658d2eb9b3b7aa897d21710f8eee5ecb269ff8d2
OKrass <OKrass@users.noreply.github.com>
2018-03-29 02:06:21 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/ConsoleServer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ConsoleServer
+{
+    public class ClientObject
+    {
+        public TcpClient client;
+        public ClientObject(TcpClient tcpClient)
+        {
+            client = tcpClient;
+        }
+
+        public void Process()
+        {
+            NetworkStream stream = null;
+            String data = null;
+            try
+            {
+                stream = client.GetStream();
+                byte[] bytes = new byte[256]; // set buffer size
+                data= null;
+                while (true)
+                {
+                    int i;
+                    //Getting message from client 
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+
+                        // Process the data sent by the client.
+                        data = data.ToUpper();
+
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                        // Send back a response.
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("Sent: {0}", data);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+                if (client != null)
+                    client.Close();
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ConsoleServer
+{
+    class Program
+    {
+        // Set Port
+        const int port = 3001;
+        static TcpListener listener;
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Change IP adress if needed
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                listener = new TcpListener(localAddr, port);
+                listener.Start();
+                Console.WriteLine("Awaiting connection...");
+
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    ClientObject clientObject = new ClientObject(client);
+
+                    // Creating new thread for a new client
+                    Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
+                    clientThread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (listener != null)
+                    listener.Stop();
+            }
+        }
+    }
+}
